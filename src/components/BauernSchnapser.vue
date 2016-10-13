13391d902bdc654e646a0f832133ae61e21145ab
Matthias <matthias.a1@gmx.at>
2016-10-13 19:00:07 +0200

    add vue router

---
+++ src/components/BauernSchnapser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bauern-schnapser">
+    soon...
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .bauern-schnapser {
+    text-align: center;
+    font-size: 1.75em;
+  }
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,7 @@
+import SingleSchnapser from '../components/SingleSchnapser.vue'
+import BauernSchnapser from '../components/BauernSchnapser.vue'
+
+export default [
+  { path: '/', component: SingleSchnapser },
+  { path: '/bauer', component: BauernSchnapser }
+]
